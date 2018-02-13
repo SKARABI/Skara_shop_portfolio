3cff6dfec0db27a342520cd2d4c4c5cc3f617f2f
SKARABI <skarabifounder@gmail.com>
2018-02-13 22:45:19 +0000

    I add a button to scroll the top

--- js/skara.js
+++ js/skara.js
@@ -5,16 +5,39 @@ $(document).scroll(function(){
     {   
        $('.nav-wrapper').css({"background":"ghostwhite"});
     } else {
-       $('.nav-wrapper').css({"background":"lightsteelblue"});
+       $('.nav-wrapper').css({"background":"antiquewhite"});
     }
     
     if($(this).scrollTop() > a)
     {   
-       $('.nav-wrapper').css({"background":"lightsteelblue"});
+       $('.nav-wrapper').css({"background":"antiquewhite"});
     } else {
        $('.nav-wrapper').css({"background":"ghostwhite"});
     }
           
+
+
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 100, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
     
 
 });
