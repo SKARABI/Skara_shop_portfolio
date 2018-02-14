f8adfcb11df7a190351fc00be3e931d9e1c53f9c
SKARABI <skarabifounder@gmail.com>
2018-02-14 16:25:16 +0000

    I changed the icon of the scrolling + the color

--- js/skara.js
+++ js/skara.js
@@ -15,23 +15,23 @@ $(document).scroll(function(){
        $('.nav-wrapper').css({"background":"ghostwhite"});
     }
           
+/* SCROLL PART */
 
-
-    if ($('#back-to-top').length) {
-        var scrollTrigger = 100, // px
+    if ($('.fa-arrow-circle-up').length) {
+        var scrollTrigger = 10, // px
             backToTop = function () {
                 var scrollTop = $(window).scrollTop();
                 if (scrollTop > scrollTrigger) {
-                    $('#back-to-top').addClass('show');
+                    $('.fa-arrow-circle-up').addClass('show');
                 } else {
-                    $('#back-to-top').removeClass('show');
+                    $('.fa-arrow-circle-up').removeClass('show');
                 }
             };
         backToTop();
         $(window).on('scroll', function () {
             backToTop();
         });
-        $('#back-to-top').on('click', function (e) {
+        $('.fa-arrow-circle-up').on('click', function (e) {
             e.preventDefault();
             $('html,body').animate({
                 scrollTop: 0
